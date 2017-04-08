5f347be8f2383d6bfaa5707f7b6a330cf1e0f21e
Shirley Xie <shirleyxiesmacpro@Shirleys-MacBook-Pro.local>
2017-04-08 14:28:45 -0700

    stored values

---
+++ liri.js
@@ -0,0 +1,21 @@
+//Twitter api keys imported from secure file (keys.js)
+var keys = require("./keys.js");
+//Loading the modules
+var fs = require('fs');
+var spotify = require('spotify');
+var request = require('request');
+var twitter = require('twitter');
+
+//Store twitter keys
+var twitterClient = new Twitter ({
+  consumer_key: keys.TWITTER.CONSUMER_KEY,
+  consumer_secret: keys.TWITTER.CONSUMER_SECRET,
+  access_token_key: keys.TWITTER.ACCESS_TOKEN_KEY,
+  access_token_secret: keys.TWITTER.ACCESS_TOKEN_SECRET
+});
+
+//Store user request and declare variables
+var command = process.argv[2],
+var arg = process.argv[3],
+var requestMsg = "",
+var responseMsg = "";
